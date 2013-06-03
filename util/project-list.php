4691d0dc9ac2b61b7582fda98a362d5766a2528b
Nick Braica <nbraica@tankdesign.com>
2013-06-03 14:50:09 -0400

    styled the input fields; added ability on project list to see what projects have outstanding balances

--- util/project-list.php
+++ util/project-list.php
@@ -11,10 +11,24 @@
 	while($row = $result->fetch_assoc()){
 		$name = $row['name'];
 		$db_name = $row['project'];
+
+		$paid_sql = "SELECT COUNT('paid') FROM  `" . $db_name . "` WHERE  `paid` = 0";
+		if(!$paid_result = $db->query($paid_sql)){
+			die('There was an error running the query [' . $db->error . ']');
+		}
+
+		while($paid_row = $paid_result->fetch_row()){
+			$paid_num = $paid_row[0];
+			$paid_class = $paid_num > 0 ? 'owed' : 'no-money';
+		
 ?>
-	<a href="addhours.php?val=<?php echo $db_name ?>"><?php echo $name ?></a>
+	<div>
+		<a class="<?php echo $paid_class; ?>" href="addhours.php?val=<?php echo $db_name ?>"><?php echo $name ?></a>
+	</div>
+	
 
 <?php
+		}
 	} 
 	
 	dbclose($result, $db);
