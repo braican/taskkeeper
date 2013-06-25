69ece715e48d7b2d3ebdc79eedcafc0f16cd6592
Nick Braica <nbraica@tankdesign.com>
2013-06-25 09:30:43 -0400

    refactored file structure. separated header and footer into includes

--- index.php
+++ index.php
@@ -1,50 +1,17 @@
-<html>
-<head>
-	<title>TASKKEEPER</title>
 
-	<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="style.css">
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-</head>
-<body>
-
-<div class="container">
+<?php include('includes/header.html'); ?>
 	
 	<h1>Projects</h1>
 	<div class="project-list">
 		<?php include("util/project-list.php"); ?>
-	</div>
+	</div><!-- .project-list -->
 
-		<form action="util/add_project.php" method="post">
-			<input type="text" name="new_project" maxlength="70" placeholder="add project"></input>
-			<input type="text" name="rate" placeholder="$" size="5"></input>
-			<input type="submit" value="Submit"></input>
-		</form>
+	<form action="util/add_project.php" method="post" id="add-project">
+		<input type="text" name="new_project" maxlength="70" placeholder="add project"></input>
+		<input type="text" name="rate" placeholder="$" size="5"></input>
+		<input type="submit" value="Submit"></input>
+	</form>
 
-		<div class="success-text"></div>
-</div>
-<script type="text/javascript">
-	$('form').on('submit', function(e){
-	    e.preventDefault();
-	    $.ajax({
-	        type     : "POST",
-	        cache    : false,
-	        url      : $(this).attr('action'),
-	        data	 : $(this).serialize(),
-	        success  : function(data) {
-	        	console.log(data);
+	<div class="success-text"></div>
 
-	            $(".success-text").empty().html(data).animate({opacity:1});
-	            setTimeout(function(){
-	            	$(".success-text").animate({opacity:0});
-	            }, 5000);
-	            $(".project-list").load("util/project-list.php");
-	        }
-	    });
-
-		$('form input[type=text]').val('');
-	});
-</script>
-
-</body>
-</html>
+<?php include('includes/footer.html'); ?>
---
+++ js/taskkeeper.js
@@ -0,0 +1,74 @@
+
+$(function(){
+
+	// get paid button
+	$('.get-paid').on('click', function(e){
+		e.preventDefault();
+		var proj = $(this).attr('data-project');
+		$.ajax({
+			type	: "POST",
+			cache	: false,
+			url		: 'util/payed_switch.php',
+			data	: 'project=' + proj,
+			success : function(data){
+				console.log(data);
+				$(".success-payed-text").empty().html("PAYED").animate({opacity:1});
+				$(".hours-list").load("util/hours-list.php?val=" + proj);
+			}
+		});
+	});
+
+	// when a row is clicked, dull it out a bit
+	$('tr').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('dulled');
+	});
+
+	// -------------------------------
+	// forms
+	//
+
+	// add project form
+	$('#add-project').on('submit', function(e){
+	    e.preventDefault();
+	    $.ajax({
+	        type     : "POST",
+	        cache    : false,
+	        url      : $(this).attr('action'),
+	        data	 : $(this).serialize(),
+	        success  : function(data) {
+	        	console.log(data);
+
+	            $(".success-text").empty().html(data).animate({opacity:1});
+	            setTimeout(function(){
+	            	$(".success-text").animate({opacity:0});
+	            }, 5000);
+	            $(".project-list").load("util/project-list.php");
+	        }
+	    });
+
+		$('form input[type=text]').val('');
+	});
+
+	// add hours form
+	$('#add-hours').on('submit', function(e){
+	    e.preventDefault();
+	    $.ajax({
+	        type     : "POST",
+	        cache    : false,
+	        url      : $(this).attr('action'),
+	        data	 : $(this).serialize(),
+	        success  : function(data) {
+	        	console.log(data);
+
+	            $(".success-text").empty().html(data).animate({opacity:1});
+	            setTimeout(function(){
+	            	$(".success-text").animate({opacity:0});
+	            }, 5000);
+	            $(".hours-list").load("util/hours-list.php?val=<?php echo $project ?>");
+
+	            $('input').val('');
+	        }
+	    });
+	});
+});
