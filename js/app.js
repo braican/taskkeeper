60169c5328a7b4c4808f13dd89ad4bf101a0d34a
Nick Braica <nbraica@tankdesign.com>
2014-06-06 16:38:09 -0400

    the basics of ember. i think i'm learning

--- js/app.js
+++ js/app.js
@@ -1,109 +1,174 @@
 App = Ember.Application.create();
 
-var data = [{
-    id: "1",
-    name: "Buddy Allen Management",
-    rate: 30,
-    waiting: false,
-    line_items: {
-        fixed: [{
-            id:"1",
-            description: "buying Futura",
-            cost: 25,
-            paid: false
-        }],
-        hourly: [{
-            id:"2",
-            description: "mobilizing the adam jensen website",
-            hours: "4",
-            paid: false
-        }, {
-            id:"3",
-            description: "new splash page",
-            hours: "1",
-            paid: false
-        }]
-    }
-}, {
-    id: "2",
-    name: "EDF",
-    rate: 0,
-    waiting: true,
-    line_items: {
-        fixed: [{
-            id:"1",
-            description: "invoice at first half",
-            cost: 925,
-            paid: false
-        },{
-            id:"2",
-            description: "purchasing the ACF Repeater plugin",
-            cost: 25,
-            paid: false
-        }],
-        hourly:[]
-    }
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+// var data = [{
+//     id: "1",
+//     name: "Buddy Allen Management",
+//     rate: 30,
+//     isOwed: false,
+//     line_items: {
+//         fixed: [{
+//             id:"1",
+//             description: "buying Futura",
+//             cost: 25,
+//             paid: false
+//         }],
+//         hourly: [{
+//             id:"2",
+//             description: "mobilizing the adam jensen website",
+//             hours: "4",
+//             paid: false
+//         }, {
+//             id:"3",
+//             description: "new splash page",
+//             hours: "1",
+//             paid: false
+//         }]
+//     }
+// }, {
+//     id: "2",
+//     name: "EDF",
+//     rate: 0,
+//     isOwed: true,
+//     line_items: {
+//         fixed: [{
+//             id:"1",
+//             description: "invoice at first half",
+//             cost: 925,
+//             paid: false
+//         },{
+//             id:"2",
+//             description: "purchasing the ACF Repeater plugin",
+//             cost: 25,
+//             paid: false
+//         }],
+//         hourly:[]
+//     }
+// }];
+
+App.Client = DS.Model.extend({
+    name: DS.attr('string'),
+    invoiced: DS.attr('boolean'),
+    moneyDue: DS.attr('number'),
+    dueDate: DS.attr('date'),
+    hourlyRate: DS.attr('number')
+});
+
+App.Client.FIXTURES = [{
+   id: 1,
+   name: 'Buddy Allen Management',
+   invoiced: false,
+   moneyDue: 0,
+   dueDate: null,
+   hourlyRate: 25
+},{
+   id: 2,
+   name: 'EDF',
+   invoiced: false,
+   moneyDue: 120,
+   dueDate: new Date('2014/06/09'),
+   hourlyRate: null
+},{
+   id: 3,
+   name: 'Brooklyn Boulders',
+   invoiced: true,
+   moneyDue: 1160,
+   dueDate: new Date('2014/06/06'),
+   hourlyRate: null
 }];
 
 
 App.Router.map(function(){
-    this.resource('about');
     this.resource('clients', {path: '/'}, function(){
         this.resource('client', { path: ':client_id' });
     });
-    
 });
 
 App.ClientsRoute = Ember.Route.extend({
     model: function(){
-        return data;
+        // return data;
+        return this.store.find('client');
     }
 });
 
-App.ClientRoute = Ember.Route.extend({
-    model: function(params){
-        return data.findBy('id', params.client_id);
-    }
-});
 
-App.ClientController = Ember.ObjectController.extend({
-    isEditingClient: false,
+//
+// controllers
+//
 
-    actions:{
-        edit: function(){
-            this.set('isEditingClient', true);
-        },
+App.ClientsController = Ember.ArrayController.extend({
+    actions: {
+        createClient: function(){
+            var name = this.get('newClient');
 
-        doneEditing: function(){
-            this.set('isEditingClient', false);
+            console.log(this);
+            if(!name.trim()){return;}
+
+            var client = this.store.createRecord('client', {
+                name: name,
+                invoiced: false,
+                moneyDue: 0,
+                dueDate: null,
+                hourlyRate: null
+            });
+
+            this.set('newClient', '');
+
+            client.save();
         }
     }
 });
 
 
-Ember.Handlebars.helper('hourlySubtotal', function(hrs){
-    var rate = 30;
-    // console.log($('#the-rate'));
-    // console.log(hrs);
-    // console.log(this);
-    return hrs * rate;
+App.ClientController = Ember.ObjectController.extend({
+
+    invoiced: function(key, value){
+        var model = this.get('model');
+
+        if(value === undefined){
+            return model.get('invoiced');
+        } else {
+            model.set('invoiced', value);
+            model.save();
+            return value;
+        }
+    }.property('model.invoiced'),
+
+    actions: {
+        editName: function(){
+            this.set('isEditing', true);
+        },
+
+        acceptChanges: function(){
+            this.set('isEditing', false);
+
+            this.get('model').save();
+        }
+    },
+
+    isEditing: false
+    
 });
 
-Ember.Handlebars.helper('totalDue', function(hrs, rate){
-    // var rate = this.content.rate,
-    //     totalDue = 0;
 
-    // $.each(this.content.line_items.hourly, function(index, val) {
-    //     totalDue += (val.hours * rate);
-    // });
+//
+// views
+//
 
-    // $.each(this.content.line_items.fixed, function(index, val) {
-    //     totalDue += val.cost;
-    // });
-
-    console.log(hrs);
-    console.log(rate);
-    // console.log(hrs);
-    // console.log(this);
-    return hrs * rate;
+App.EditNameView = Ember.TextField.extend({
+    didInsertElement: function(){
+        this.$().focus();
+    }
 });
+
+//
+// handlebars helpers
+//
+Ember.Handlebars.helper('edit-name', App.EditNameView);
+
+
+Ember.Handlebars.helper('parseDate', function(date){
+    console.log(date);
+    return date ? date.toDateString() : '';
+});
+
