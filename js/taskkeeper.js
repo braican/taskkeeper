318c9509de6ca12c64415d992788564e1cc2f8ea
braican <nick.braica@gmail.com>
2015-09-09 19:23:30 -0400

    setting up gulp cause why not

--- js/taskkeeper.js
+++ js/taskkeeper.js
@@ -1,4 +1,14 @@
 (function(){
-    var app = angular.module('taskkeeper', ["firebase"]);
+    var app = angular.module('taskkeeper', []);
+    Parse.initialize("eqWRtTyxO7URAQU5ExyWkbg2D3VAeyN05O59xYAY", "xPAmUBGaDB5mkC876RW39gG4WDTeTfVC5oc4iwzU");
+
+    var Clients = Parse.Object.extend("Client");
+    var query = new Parse.Query(Clients);
+
+    query.find({
+        success: function(results){
+            console.log(results[0].get('hourlyRate'));
+        }
+    });
 
 })();
