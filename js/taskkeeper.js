e09bf11c34dae3cbe94aed7cf97871e738f80e75
braican <nick.braica@gmail.com>
2015-10-12 19:10:50 -0400

    some learnings

--- js/taskkeeper.js
+++ js/taskkeeper.js
@@ -1,14 +1,30 @@
 (function(){
     var app = angular.module('taskkeeper', []);
+
     Parse.initialize("eqWRtTyxO7URAQU5ExyWkbg2D3VAeyN05O59xYAY", "xPAmUBGaDB5mkC876RW39gG4WDTeTfVC5oc4iwzU");
 
     var Clients = Parse.Object.extend("Client");
     var query = new Parse.Query(Clients);
 
-    query.find({
-        success: function(results){
-            console.log(results[0].get('hourlyRate'));
-        }
+    // query.find({
+    //     success: function(results){
+    //         angular.forEach(results, function(result, key){
+    //             var clientName = result.get('name'),
+    //                 hourlyRate = result.get('hourlyRate');
+    //         });
+    //     }
+    // });
+
+    var clientsData = [{
+        name: "Christa Bianchi",
+        hourlyRate: 52
+    },{
+        name: "EDF",
+        hourlyRate: 45
+    }];
+
+    app.controller("ClientController", function(){
+        this.clients = clientsData;
     });
 
 })();
