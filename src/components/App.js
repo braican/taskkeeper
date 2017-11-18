ac8e8229883ed61344d4b22cafca398adbdc1a86
Nick Braica <nick.braica@gmail.com>
2017-11-18 17:00:55 -0500

    test app wooo

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className="test">This is a sweet test</div>
+        );
+    }
+}
+
+export default App;
