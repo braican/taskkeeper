5c99f871bd335b069d26f2c913d3306510e34176
Nick Braica <nick.braica@gmail.com>
2018-03-29 12:48:26 -0400

    style the invoice add button

--- src/components/OpenTasks.js
+++ src/components/OpenTasks.js
@@ -5,30 +5,42 @@ import PropTypes from 'prop-types';
 import TaskList from './TaskList';
 
 
+
 const OpenTasks = (props) => {
     const notasksCopy = 'You have no billable tasks for this client.';
     const taskCount = props.tasks.length;
 
+    /**
+     * Submit the invoice
+     * @param {Object} e Event data
+     */
     const submitInvoice = (e) => {
         e.preventDefault();
         props.submitInvoice(props.tasks);
     };
 
+    /**
+     * Render the submit invoice button
+     */
+    const renderSubmitInvoiceBtn = () => (
+        <div className="l-container l-space-top l-align-right">
+            <button className="tk-btn tk-btn--submit" onClick={(e) => submitInvoice(e)}>Submit invoice</button>
+        </div>
+    );
+
     return (
         <section className="client__opentasks">
-            <div className="l-container">
-                {
-                    taskCount > 0 ?
-                        <TaskList tasks={props.tasks} rate={props.rate} /> :
-                        <p>{notasksCopy}</p>
-                }
+            <div className="opentasks">
+                <div className="l-container">
+                    {
+                        taskCount > 0 ?
+                            <TaskList tasks={props.tasks} rate={props.rate} /> :
+                            <p>{notasksCopy}</p>
+                    }
+                </div>
             </div>
 
-            {
-                taskCount > 0 ?
-                    <button onClick={(e) => submitInvoice(e)}>Submit invoice</button> :
-                    null
-            }
+            {taskCount > 0 ? renderSubmitInvoiceBtn() : null}
         </section>
     );
 };
