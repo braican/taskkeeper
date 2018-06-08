01ec1eff75c643cf842acb326302e759ca1cde27
Nick Braica <nick.braica@gmail.com>
2018-06-08 17:09:55 -0400

    Setup for master

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/ braican@69.164.211.156:~/_tmp/taskkeeper
 
 # ssh 
-ssh braican@69.164.211.156 "mv ~/_tmp/taskkeeper /var/www/taskkeeper; rm -rf ~/_tmp/taskkeeper"
+ssh braican@69.164.211.156 "mv ~/_tmp/taskkeeper /var/www; rm -rf ~/_tmp/taskkeeper"
