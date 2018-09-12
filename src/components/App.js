42394d824737d84e9b214bdf2d4feb6d3aa783e2
Nick Braica <nick.braica@gmail.com>
2018-09-12 13:30:44 -0500

    Setup of linters and configs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 
-
 class App extends React.Component {
     // render function
     render() {
-        return (
-            <h1>Test</h1>
-        );
+        return <h1>Test</h1>;
     }
 }
 
