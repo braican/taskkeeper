ec329c84417e1b2dd39d631327eace5f33fe0d7c
Nick Braica <nick.braica@gmail.com>
2019-09-10 14:18:12 -0700

    Update client data

--- src/components/Client/index.js
+++ src/components/Client/index.js
@@ -4,7 +4,7 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { isLoaded, firestoreConnect } from 'react-redux-firebase';
 
-import { clientFilter } from '../../utils';
+import { clientFilter, className } from '../../utils';
 import { invoice as invoiceStatus } from '../../utils/status';
 
 import FormattedPrice from '../Utils/FormattedPrice';
@@ -14,29 +14,50 @@ import EstimatedTasks from './EstimatedTasks';
 import CompletedTasks from './CompletedTasks';
 import ActiveInvoices from './ActiveInvoices';
 import ArchivedInvoices from './ArchivedInvoices';
+import FormEl from '../Forms/FormEl';
 import FadeIn from '../Transitions/FadeIn';
+import GearIcon from '../../svg/Gear';
 
 import styles from './Client.module.scss';
 
 export const ClientContext = React.createContext();
 
 const Client = ({
-  client: { symbol, name, rate, id },
+  client,
   activeInvoices,
   estimatedTasks,
   completedTasks,
   invoicedTasks,
   archivedInvoices,
   unsetInvoicing,
+  userRef,
 }) => {
   const [nextInvoiceId, setNextInvoiceId] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [address, setAddress] = useState(client.address);
+  const [symbol, setSymbol] = useState(client.symbol);
+  const [updated, setUpdated] = useState(false);
+
+  const handleSave = () => {
+    setIsEditing(false);
+
+    if (!userRef || !updated) {
+      return;
+    }
+
+    setUpdated(false);
+    userRef
+      .collection('clients')
+      .doc(client.id)
+      .update({ address, symbol });
+  };
 
   return (
     <ClientContext.Provider
       value={{
-        id,
+        id: client.id,
         symbol,
-        rate,
+        rate: client.rate,
         nextInvoiceId,
         setNextInvoiceId,
       }}>
@@ -46,11 +67,52 @@ const Client = ({
         </BackLink>
 
         <header className={styles.client__header}>
-          <h2 className={styles.client__name}>{name}</h2>
+          <div className={styles.header__left}>
+            <h2 className={styles.client__name}>{client.name}</h2>
 
-          <p>
-            <FormattedPrice price={rate} />
-          </p>
+            <p>
+              <FormattedPrice price={client.rate} />
+            </p>
+          </div>
+
+          <div {...className('stack', styles.header__right, isEditing && styles.editingClient)}>
+            <FormEl
+              id="client-address"
+              type="textarea"
+              label="Address"
+              className={styles.addressInput}
+              value={address}
+              onChange={event => {
+                setAddress(event.target.value);
+                setUpdated(true);
+              }}
+              inputConfig={isEditing ? {} : { disabled: true }}
+              absoluteLabel
+            />
+
+            <FormEl
+              id="client-symbol"
+              label="Symbol"
+              className={styles.symbolInput}
+              value={symbol}
+              onChange={event => {
+                setSymbol(event.target.value);
+                setUpdated(true);
+              }}
+            />
+          </div>
+
+          <div className={styles.editActions}>
+            {isEditing ? (
+              <button onClick={handleSave} className="button">
+                Save
+              </button>
+            ) : (
+              <button className={styles.editClient} onClick={() => setIsEditing(true)}>
+                <GearIcon />
+              </button>
+            )}
+          </div>
         </header>
 
         <FadeIn in={activeInvoices && activeInvoices.length > 0}>
@@ -86,6 +148,7 @@ Client.propTypes = {
     name: PropTypes.string,
     rate: PropTypes.string,
     id: PropTypes.string,
+    address: PropTypes.string,
   }).isRequired,
   activeInvoices: PropTypes.array,
   estimatedTasks: PropTypes.array,
@@ -93,6 +156,7 @@ Client.propTypes = {
   invoicedTasks: PropTypes.object,
   archivedInvoices: PropTypes.array,
   unsetInvoicing: PropTypes.func.isRequired,
+  userRef: PropTypes.object,
 };
 
 Client.defaultProps = {
@@ -101,11 +165,12 @@ Client.defaultProps = {
   completedTasks: [],
   invoicedTasks: {},
   archivedInvoices: [],
+  userRef: null,
 };
 
 export default compose(
   connect(
-    ({ firestore, firebase: { auth } }, props) => {
+    ({ firestore, firebase: { auth }, userRef }, props) => {
       const { clientId: id } = props.match.params;
       const {
         activeInvoices: allActiveInvoices,
@@ -135,6 +200,7 @@ export default compose(
         completedTasks,
         invoicedTasks,
         archivedInvoices,
+        userRef,
       };
     },
     dispatch => ({ unsetInvoicing: () => dispatch({ type: 'UNSET_INVOICING' }) }),
@@ -152,7 +218,7 @@ export default compose(
           {
             collection: 'invoices',
             where: [['status', '==', invoiceStatus.FULFILLED], ['client', '==', client.id]],
-            orderBy: [['fulfilledDate']],
+            orderBy: [['fulfilledDate'], ['invoiceId']],
           },
         ],
         storeAs: `${client.id}_invoices`,
