c11726304fb3a11eacd833f71d5ecc21c2a50b24
Nick Braica <nick.braica@gmail.com>
2021-05-18 20:38:53 -0700

    A bunch of design

--- src/components/elements/Task/index.js
+++ src/components/elements/Task/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import ContentEditable from 'react-contenteditable';
 import { useClients } from 'hooks';
 
 import Icon from 'components/ui/Icon';
@@ -7,33 +8,67 @@ import Icon from 'components/ui/Icon';
 import styles from './Task.module.scss';
 
 const Task = ({ task }) => {
-  const [open, setOpen] = useState(false);
+  const initialDescription = task.description;
+  const initialHours = task.hours;
+
+  const description = useRef(task.description);
+  const [hours, setHours] = useState(task.hours);
+  const hoursInput = useRef();
+
   const {
     client: { rate },
   } = useClients();
 
-  return (
-    <>
-      <button type="button" className={styles.taskData} onClick={() => setOpen(!open)}>
-        <span className={styles.description}>{task.description}</span>
+  const handleDescriptionBlur = event => {
+    console.log(event.target.innerHTML);
+  };
+  const handleHoursBlur = event => {
+    console.log(hours);
+  };
 
-        {task.price === undefined && <span className={styles.hours}>{task.hours} hours</span>}
+  return (
+    <div className={styles.task}>
+      <div type="button" className={styles.taskData}>
+        <ContentEditable
+          html={description.current}
+          className={styles.description}
+          tagName="p"
+          onBlur={handleDescriptionBlur}
+        />
+
+        {task.price === undefined && (
+          <span className={styles.hoursWrap}>
+            <button
+              className={styles.hoursLabel}
+              type="button"
+              onClick={() => hoursInput?.current?.focus()}>
+              hours:
+            </button>
+            <input
+              type="number"
+              value={hours}
+              ref={hoursInput}
+              onBlur={handleHoursBlur}
+              className={styles.hours}
+              onChange={event => setHours(event.target.value)}
+            />
+          </span>
+        )}
 
         <span className={styles.price}>
           ${task.price === undefined ? parseFloat(task.hours) * rate : task.price}
         </span>
-      </button>
-      {open && (
-        <div className={styles.utility}>
-          <button type="button" className={styles.trash}>
-            <Icon label="Delete" viewBox="0 0 20 20" icon="trash" />
-          </button>
-          <button type="button" className={styles.move}>
-            <Icon label="To do" viewBox="0 0 20 20" icon="cheveron-down" />
-          </button>
-        </div>
-      )}
-    </>
+      </div>
+
+      <div className={styles.utility}>
+        <button type="button" className={styles.trash}>
+          <Icon label="Delete" viewBox="0 0 20 20" icon="trash" inline />
+        </button>
+        <button type="button" className={styles.move}>
+          <Icon label="To do" viewBox="0 0 20 20" icon="cheveron-down" inline />
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -7,22 +7,24 @@ import styles from './Icon.module.scss';
  * @param {string} label
  * @param {string} viewBox
  * @param {string} icon
+ * @param {boolean} inline
  *
  * @return jsx
  */
-const Icon = ({ label = '', viewBox = '', icon }) => (
-  <>
+const Icon = ({ label = '', viewBox = '', icon, inline = false }) => (
+  <div className={inline ? styles.inline : ''}>
     <svg className={styles.svg} viewBox={viewBox}>
       <use xlinkHref={`#icon-${icon}`}></use>
     </svg>
     {label && <span className={styles.label}>{label}</span>}
-  </>
+  </div>
 );
 
 Icon.propTypes = {
   label: PropTypes.string,
   viewBox: PropTypes.string,
   icon: PropTypes.string.isRequired,
+  inline: PropTypes.bool,
 };
 
 export default Icon;
