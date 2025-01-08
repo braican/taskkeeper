0874771c75260d343a085d23999118d8b701f85f
Nick Braica <nick.braica@gmail.com>
2025-01-08 16:11:19 -0500

    Clear out the old

--- src/providers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as AuthProvider } from './AuthProvider';
-export { default as ClientProvider } from './ClientProvider';
-export { default as TaskProvider } from './TaskProvider';
-export { default as ProjectProvider } from './ProjectProvider';
-export { default as NewInvoiceProvider } from './NewInvoiceProvider';
-export { default as InvoiceProvider } from './InvoiceProvider';
