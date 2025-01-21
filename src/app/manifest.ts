e98018a27cdda947e56aac36d7d8df9dbd30ddcd
Nick Braica <nick.braica@gmail.com>
2025-01-21 12:58:02 -0500

    More icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,12 +9,32 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#004952',
     theme_color: '#f7f6f7',
+    screenshots: [
+      {
+        src: '/icons/tk-wide.png',
+        sizes: '1280x720',
+        type: 'image/png',
+        form_factor: 'wide',
+      },
+      {
+        src: '/icons/tk-tall.png',
+        sizes: '393x852',
+        type: 'image/png',
+        form_factor: 'narrow',
+      },
+    ],
     icons: [
       {
         src: '/icons/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
+      },
+      {
+        src: '/icons/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
       },
       {
         src: '/icons/web-app-manifest-512x512.png',
