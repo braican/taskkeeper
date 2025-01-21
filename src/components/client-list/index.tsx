8d8ca4ca4cad01edbc51b76b5d65d917dc9fb672
Nick Braica <nick.braica@gmail.com>
2025-01-21 11:12:42 -0500

    handle client list

--- src/components/client-list/index.tsx
+++ src/components/client-list/index.tsx
@@ -1,9 +1,39 @@
-import { useClients } from '@/contexts/ClientContext';
 import Link from 'next/link';
+import { useClients } from '@/contexts/ClientContext';
+import { useTasks } from '@/contexts/TaskContext';
+import { useInvoices } from '@/contexts/InvoiceContext';
+
 import styles from './client-list.module.css';
 
 export default function ClientList() {
   const { clients, areClientsLoaded } = useClients();
+  const { getClientTasks } = useTasks();
+  const { getClientInvoices, getActiveInvoices } = useInvoices();
+  const sortedClients = clients
+    .map((client) => {
+      const { activeInvoices } = getClientInvoices(client.id);
+      const { pending, sent } = getActiveInvoices(activeInvoices);
+      const activeTasks = getClientTasks(client.id);
+
+      return {
+        ...client,
+        activeTasks,
+        activePending: pending,
+        activeSent: sent,
+        hasStatus:
+          activeTasks.length > 0 || pending.length > 0 || sent.length > 0,
+      };
+    })
+    .sort((a, b) => {
+      if (a.hasStatus && !b.hasStatus) {
+        return -1;
+      }
+      if (!a.hasStatus && b.hasStatus) {
+        return 1;
+      }
+
+      return a.name.localeCompare(b.name);
+    });
 
   if (!areClientsLoaded) {
     return <p>Loading clients...</p>;
@@ -15,15 +45,40 @@ export default function ClientList() {
 
   return (
     <ul className={`ul-reset ${styles.list}`}>
-      {clients.map((client) => (
+      {sortedClients.map((client) => (
         <li key={client.id} className={styles.item}>
-          <Link href={`/client/${client.id}`} className={styles.link}>
+          <Link
+            href={`/client/${client.id}`}
+            className={`${styles.link} ${client.hasStatus ? styles.hasStatus : ''}`}
+          >
             <div className={styles.inner}>
-              <div className="client-superscript">
-                <h3 className={styles.name}>{client.name}</h3>
-                <p>{client.key}</p>
+              <div className={styles.clientMain}>
+                <div>
+                  <h3 className={styles.name}>{client.name}</h3>
+                  <p className={styles.key}>&nbsp;&nbsp;{client.key}</p>
+                </div>
+                <p>${client.rate}/hour</p>
               </div>
-              <p>${client.rate}/hour</p>
+              <div className={styles.statuses}>
+                {client.activeTasks.length > 0 && (
+                  <p>
+                    {client.activeTasks.length} open task
+                    {client.activeTasks.length === 1 ? '' : 's'}
+                  </p>
+                )}
+                {client.activePending.length > 0 && (
+                  <p>
+                    {client.activePending.length} invoice
+                    {client.activePending.length === 1 ? '' : 's'} pending
+                  </p>
+                )}
+                {client.activeSent.length > 0 && (
+                  <p>
+                    {client.activeSent.length} invoice
+                    {client.activeSent.length === 1 ? '' : 's'} awaiting payment
+                  </p>
+                )}
+              </div>
             </div>
           </Link>
         </li>
